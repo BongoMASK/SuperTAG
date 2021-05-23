8b1e7f80fdc6633ca2bf8f9c6899cb4dd6868a96
BongoMASK <67361918+BongoMASK@users.noreply.github.com>
2021-05-23 16:39:18 +0530

    lighting

--- Assets/Scripts/ShapeMaker/Editor/PathEditor.cs
+++ Assets/Scripts/ShapeMaker/Editor/PathEditor.cs
@@ -29,9 +29,13 @@ public class PathEditor : Editor {
         for (int i = 0; i < path.NumSegments; i++) {
             Vector2[] points = path.GetPointsInSegment(i);
             Handles.color = Color.black;
-            Handles.DrawLine(points[1], points[0]);
-            Handles.DrawLine(points[2], points[3]);
-            Handles.DrawBezier(points[0], points[3], points[1], points[2], Color.green, null, 2);
+            //Handles.DrawLine(points[1], points[0]);
+            //Handles.DrawLine(points[2], points[3]);
+            //Handles.DrawBezier(points[0], points[3], points[1], points[2], Color.green, null, 2);
+            for (int j = 0; j < points.Length; j++) {
+                if(j+3 < points.Length)
+                    Handles.DrawLine(points[j], points[j + 3]);
+            }
         }
 
         Handles.color = Color.red;
